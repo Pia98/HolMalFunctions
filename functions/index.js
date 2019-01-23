8de1e357c3816ce8279dc3f66980d3a193bcb7e0
Pia Probst <pia.probst@online.de>
2019-01-23 21:40:36 +0100

    Deleting unnecessary test function

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   console.log('helloWorld', 'Hello World!');
-  response.send("Hello from Firebase!");
- });
-
 exports.deleteItemDoneTrue = functions.database.ref('/item/{itemId}')
 .onUpdate((change, context) => {
   const itemId = context.params.itemId;
