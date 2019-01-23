c6cf67bed0634eb1b907fb72d6e6166649375f49
Pia Probst <pia.probst@online.de>
2019-01-23 19:10:14 +0100

    first try (without timestamp)

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,20 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+   console.log('helloWorld', 'Hello World!');
+  response.send("Hello from Firebase!");
+ });
+
+exports.deleteItemDoneTrue = functions.database.ref('/item/{itemId}')
+.onUpdate((change, context) => {
+  const itemId = context.params.itemId;
+  const before = change.before.val();
+  const after = change.after.val();
+  console.log('deleteItemDoneTrue', `ItemID: ${itemId} -
+    its before value: ${before} -
+    its after value: ${after}`);
+  if((before.done === false) && (after.done === true)){
+    return admin.database.ref('/item/' + itemId).remove();
+  }else return null;
+})
